f46591f4a835f643d787b2ee3a7001c624991a62
TadeQ <88510018+TadeQQ@users.noreply.github.com>
2023-11-24 20:17:30 +0100

    feat: update files

--- Program.cs
+++ Program.cs
@@ -1,34 +1,47 @@
-﻿class Program
+﻿using System;
+using System.Linq;
+
+public class Program
 {
-    static void Main(string[] args)
-    {   
-        Console.WriteLine("Podaj swoje Nazwisko, Wiek oraz Płeć (wpisz: Mężczyzna lub Kobieta): ");
-        Console.WriteLine("Nazwisko: ");
-        string LastName = Console.ReadLine();
-        
-        Console.WriteLine("Wiek: ");
-        int currAge = Convert.ToInt32(Console.ReadLine());
-        
-        Console.WriteLine("Płeć: ");
-        string gender = Console.ReadLine();
-
-        int maleRetireAge = 65;
-        int femaleRetireAge = 60;
-
-        Console.WriteLine($"Witaj, {LastName}!");
-        
-        if (currAge < 0) {
-        Console.WriteLine("Wiek nie może być ujemny!");
-     } 
-        else if (gender == "Kobieta" && currAge < femaleRetireAge)
-     { Console.WriteLine($"Masz {currAge} lat i jesteś {gender.Remove(gender.Length -1) + "ą"}, więc brakuje Ci {femaleRetireAge - currAge } lat do emerytury!");
-     } 
-        else if (gender == "Mężczyzna" && currAge < maleRetireAge)
-     { Console.WriteLine($"Masz {currAge} lat i jesteś {gender.Remove(gender.Length -1) + "ą"}, więc brakuje Ci {maleRetireAge - currAge } lat do emerytury!");
-     } 
-        else {Console.WriteLine("Jesteś emerytem!");
-         }
+    public static void Main()
+    {
+        for (int A = 1; A < 10; A++)
+        {
+            for (int B = 0; B < 10; B++)
+            {
+                if (B == A) continue;
+                for (int C = 0; C < 10; C++)
+                {
+                    if (C == A || C == B) continue;
+                    for (int D = 0; D < 10; D++)
+                    {
+                        if (D == A || D == B || D == C) continue;
+                        for (int E = 0; E < 10; E++)
+                        {
+                            if (E == A || E == B || E == C || E == D) continue;
+                            for (int F = 1; F < 10; F++)
+                            {
+                                if (F == A || F == B || F == C || F == D || F == E) continue;
+                                for (int G = 0; G < 10; G++)
+                                {
+                                    if (G == A || G == B || G == C || G == D || G == E || G == F) continue;
+                                    for (int H = 0; H < 10; H++)
+                                    {
+                                        if (H == A || H == B || H == C || H == D || H == E || H == F || H == G) continue;
+                                        int num1 = 10000 * A + 1000 * B + 100 * C + 10 * D + E;
+                                        int num2 = 100 * F + 10 * G + A;
+                                        int result = num1 * num2;
+                                        if (result == 1000000 * F + 100000 * D + 10000 * E + 1000 * D + 100 * H + 10 * H + H)
+                                        {
+                                            Console.WriteLine($"A={A}, B={B}, C={C}, D={D}, E={E}, F={F}, G={G}, H={H}");
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
-
-
